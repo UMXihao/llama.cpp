d3b2faa3618775dc94c1d93f1e1b353ba11666b1
sunxihao <sunxh2016@lzu.edu.cn>
2024-12-12 19:06:23 +0800

    parallel thread decoding failed, code revert

--- common/common.cpp
+++ common/common.cpp
@@ -825,9 +825,7 @@ struct llama_init_result llama_init_from_gpt_params(gpt_params & params) {
             tmp.push_back(decoder_start_token_id);
         }
         if (llama_model_has_decoder(model)) {
-            llama_batch origin_batch = llama_batch_get_one(tmp.data(), std::min(tmp.size(), (size_t) params.n_batch), 0, 0);
-            LOG("origin_batch: %d", origin_batch.type);
-            llama_decode(lctx, origin_batch);
+            llama_decode(lctx, llama_batch_get_one(tmp.data(), std::min(tmp.size(), (size_t) params.n_batch), 0, 0));
         }
         llama_kv_cache_clear(lctx);
         llama_synchronize(lctx);
